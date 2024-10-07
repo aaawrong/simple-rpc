6f7b529a452a7be2b2c8691b358c741c857f554e
majiayong <aceshighbeita@gmail.com>
2024-10-07 15:35:35 +0800

    feat: 客户端连接超时

---
+++ day4/main/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+
+	"geerpc"
+)
+
+// 定义结构体 Foo 和方法 Sum
+type Foo int
+
+type Args struct{ Num1, Num2 int }
+
+func (f Foo) Sum(args Args, reply *int) error {
+	*reply = args.Num1 + args.Num2
+	return nil
+}
+
+func startServer(addr chan string) {
+	var foo Foo
+	if err := geerpc.Register(&foo); err != nil {
+		log.Fatal("register error:", err)
+	}
+	// 随机选择端口监听
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	geerpc.Accept(l)
+}
+
+func main() {
+	log.SetFlags(0) //不输出时间
+	addr := make(chan string)
+	go startServer(addr)
+
+	client, _ := geerpc.Dial("tcp", <-addr)
+	defer func() {
+		_ = client.Close()
+	}()
+
+	time.Sleep(time.Second)
+	// 服务端向客户端发送请求
+	//var wg sync.WaitGroup
+	//for i := 0; i < 5; i++ {
+	//	wg.Add(1)
+	//	go func(i int) {
+	//		defer wg.Done()
+	//		//args := fmt.Sprintf("geerpc req %d", i)
+	//		//var reply string
+	//		args := &Args{Num1: i, Num2: i * i}
+	//		var reply int
+	//		if err := client.Call("Foo.Sum", args, &reply); err != nil {
+	//			log.Fatal("call Foo.Sum error:", err)
+	//		}
+	//		//log.Println("reply:", reply)
+	//		log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+	//	}(i)
+	//}
+	//wg.Wait()
+
+	//args := fmt.Sprintf("geerpc req %d", 11)
+	args := &Args{Num1: 1, Num2: 3}
+	var reply int
+	if err := client.Call("Foo.Sum", args, &reply); err != nil {
+		log.Fatal("call Foo.Sum error:", err)
+	}
+	log.Printf("%d + %d = %d", args.Num1, args.Num2, reply)
+
+}
---
+++ day4/server.go
@@ -0,0 +1,208 @@
+package geerpc
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"reflect"
+	"strings"
+	"sync"
+	"time"
+
+	"geerpc/codec"
+)
+
+const MagicNumber = 0x3bef5c
+
+// 客户端与服务端的通信需要协商一些内容，例如 HTTP 报文，分为 header 和 body 2 部分，
+// body 的格式和长度通过 header 中的 Content-Type 和 Content-Length 指定，
+// 服务端通过解析 header 就能够知道如何从 body 中读取需要的信息。
+// 对于 RPC 协议来说，这部分协商是需要自主设计的。
+// | Option{MagicNumber: xxx, CodecType: xxx} | Header{ServiceMethod ...} | Body interface{} |
+// | <------      固定 JSON 编码      ------>  | <-------   编码方式由 CodeType 决定   ------->|
+// 服务端首先使用 JSON 解码 Option，然后通过 Option 的 CodeType 解码剩余的内容
+// | Option | Header1 | Body1 | Header2 | Body2 | ...
+type Option struct {
+	MagicNumber    int           // MagicNumber marks this's a geerpc request
+	CodecType      codec.Type    // client may choose different Codec to encode body
+	ConnectTimeout time.Duration // 0 means no limit
+	HandleTimeout  time.Duration
+}
+
+var DefaultOption = &Option{
+	MagicNumber:    MagicNumber,
+	CodecType:      codec.JsonType,
+	ConnectTimeout: time.Second * 10,
+}
+
+// Server represents an RPC Server.
+type Server struct {
+	serviceMap sync.Map
+}
+
+func NewServer() *Server {
+	return &Server{}
+}
+
+// DefaultServer is the default instance of *Server.
+var DefaultServer = NewServer()
+
+func Accept(lis net.Listener) {
+	DefaultServer.Accept(lis)
+}
+
+func (server *Server) Accept(lis net.Listener) {
+	for {
+		conn, err := lis.Accept()
+		if err != nil {
+			log.Println("rpc server: accept error:", err)
+			return
+		}
+		go server.ServeConn(conn)
+	}
+}
+
+func (server *Server) ServeConn(conn io.ReadWriteCloser) {
+	var opt Option
+	//收到服务端发来的opt进行解析
+	if err := json.NewDecoder(conn).Decode(&opt); err != nil {
+		log.Println("rpc server: options error: ", err)
+		return
+	}
+	//检查 MagicNumber 和 CodeType 的值是否正确
+	if opt.MagicNumber != MagicNumber {
+		log.Printf("rpc server: invalid magic number %x", opt.MagicNumber)
+		return
+	}
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		log.Printf("rpc server: invalid codec type %s", opt.CodecType)
+		return
+	}
+	server.serveCodec(f(conn))
+}
+
+func Register(rcvr interface{}) error {
+	return DefaultServer.Register(rcvr)
+}
+
+// 服务注册
+func (server *Server) Register(rcvr interface{}) error {
+	s := newService(rcvr)
+	if _, dup := server.serviceMap.LoadOrStore(s.name, s); dup {
+		return errors.New("rpc: service already defined: " + s.name)
+	}
+	return nil
+}
+
+// invalidRequest is a placeholder for response argv when error occurs
+var invalidRequest = struct{}{}
+
+func (server *Server) serveCodec(cc codec.Codec) {
+	sending := new(sync.Mutex) // 处理请求是并发的，但是回复请求的报文必须是逐个发送的，并发容易导致多个回复报文交织在一起，客户端无法解析。在这里使用锁(sending)保证。
+	wg := new(sync.WaitGroup)  // wait until all request are handled
+
+	//在一次连接中，允许接收多个请求，即多个 request header 和 request body
+	for {
+		req, err := server.readRequest(cc)
+		if err != nil {
+			if req == nil {
+				break // it's not possible to recover, so close the connection
+			}
+			req.h.Error = err.Error()
+			server.sendResponse(cc, req.h, invalidRequest, sending)
+			continue
+		}
+		wg.Add(1)
+		go server.handleRequest(cc, req, sending, wg)
+	}
+}
+
+type request struct {
+	h            *codec.Header // header of request
+	argv, replyv reflect.Value // argv and replyv of request
+	mtype        *methodType
+	svc          *service
+}
+
+// readRequest 读取请求
+func (server *Server) readRequest(cc codec.Codec) (*request, error) {
+	h, err := server.readRequestHeader(cc)
+	if err != nil {
+		return nil, err
+	}
+	req := &request{h: h}
+	req.svc, req.mtype, err = server.findService(h.ServiceMethod)
+	if err != nil {
+		return req, nil
+	}
+	//req.argv = reflect.New(reflect.TypeOf(""))
+	req.argv = req.mtype.newArgv()
+	req.replyv = req.mtype.newReplyv()
+
+	// make sure that argvi is a pointer, ReadBody need a pointer as parameter
+	argvi := req.argv.Interface()
+	if req.argv.Type().Kind() != reflect.Ptr {
+		argvi = req.argv.Addr().Interface() // 获取内存地址
+	}
+	if err = cc.ReadBody(argvi); err != nil {
+		log.Println("rpc server: read argv err:", err)
+		return req, err
+	}
+	return req, nil
+}
+
+func (server *Server) readRequestHeader(cc codec.Codec) (*codec.Header, error) {
+	var h codec.Header
+	if err := cc.ReadHeader(&h); err != nil {
+		if err != io.EOF && err != io.ErrUnexpectedEOF {
+			log.Println("rpc server: read header error:", err)
+		}
+		return nil, err
+	}
+	return &h, nil
+}
+
+// 通过 ServiceMethod 从 serviceMap 中找到对应的 service
+func (server *Server) findService(serviceMethod string) (svc *service, mtype *methodType, err error) {
+	dot := strings.LastIndex(serviceMethod, ".")
+	if dot < 0 {
+		return nil, nil, fmt.Errorf("rpc server: service/method request %s is invalid", serviceMethod)
+	}
+	serviceName, methodName := serviceMethod[:dot], serviceMethod[dot+1:]
+	svci, ok := server.serviceMap.Load(serviceName)
+	if !ok {
+		err = errors.New("rpc server: can't find service " + serviceName)
+		return
+	}
+	svc = svci.(*service)
+	mtype = svc.method[methodName]
+	if mtype == nil {
+		err = errors.New("rpc server: can't find method " + methodName)
+	}
+	return
+}
+
+// handleRequest 处理请求
+func (server *Server) handleRequest(cc codec.Codec, req *request, sending *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+	err := req.svc.call(req.mtype, req.argv, req.replyv)
+	if err != nil {
+		req.h.Error = err.Error()
+		server.sendResponse(cc, req.h, invalidRequest, sending)
+		return
+	}
+	server.sendResponse(cc, req.h, req.replyv.Interface(), sending)
+}
+
+// sendResponse 回复请求
+func (server *Server) sendResponse(cc codec.Codec, h *codec.Header, body interface{}, sending *sync.Mutex) {
+	sending.Lock()
+	defer sending.Unlock()
+	if err := cc.Write(h, body); err != nil {
+		log.Println("rpc server: write response error:", err)
+	}
+}
